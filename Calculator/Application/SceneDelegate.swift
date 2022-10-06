9c9784676e81406fd5a25de58bcf360133435cc7
Sergei Smirnov <lam97@yandex.ru>
2022-10-06 18:28:15 +0300

    start

---
+++ Calculator/Application/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  Calculator
+//
+//  Created by Сергей Смирнов on 06.10.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+    
+
+
+}
+
